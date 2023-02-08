4645cc3c9625245b30278f0d6e7dba3062c32e02
fahrurozi <m.fahruu.rozii@gmail.com>
2023-02-08 12:12:31 +0700

    try fix cors

--- app.js
+++ app.js
@@ -24,37 +24,13 @@ const verifyToken = require("./middlewares/verifyToken");
 const can = require("./middlewares/permission");
 
 const app = express();
-var allowedOrigins = ["http://localhost:3000", "*"];
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
